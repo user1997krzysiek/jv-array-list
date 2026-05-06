b523b6560cc2d6a3b561814e160b2db1914db8d2
Krzysiek <krzysztof.orawski@wp.pl>
2026-05-06 20:58:57 +0200

    ArrayList

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,29 +1,73 @@
 package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
+    private static final int INITIAL_CAPACITY = 10;
+
+    private int size;
+    private Object[] elements;
+
+    public ArrayList() {
+        elements = new Object[INITIAL_CAPACITY];
+    }
+
     @Override
     public void add(T value) {
-
+        ensureCapacity(size + 1);
+        elements[size++] = value;
     }
 
     @Override
     public void add(T value, int index) {
+        rangeCheckForAdd(index);
+        ensureCapacity(size + 1);
+        if (index < size) {
+            System.arraycopy(elements, index, elements, index + 1, size - index);
+        }
+        elements[index] = value;
+        size++;
+    }
+    private void rangeCheckForAdd(int index) {
+        if (0 > index || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
 
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity <= elements.length) {
+            return;
+        }
+        int newCapacity = (int) (elements.length * 1.5);
+        if (newCapacity < minCapacity) {
+            newCapacity = minCapacity;
+        }
+        Object[] newArray = new Object[newCapacity];
+        System.arraycopy(elements, 0, newArray, 0, size);
+        elements = newArray;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (0 > index || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+
+        }
+        return (T) elements[index];
     }
 
     @Override
     public void set(T value, int index) {
+        if (0 > index || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Index: " + index + ", Size: " + size);
 
+        }
+        elements[index] = value;
     }
 
     @Override
@@ -38,11 +82,12 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+
     }
 }
